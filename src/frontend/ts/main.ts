f460ec39c04ce9e90854e2b89cfe942a4118f212
ldidone <lucas_crar14@hotmail.com>
2023-06-12 21:15:30 -0300

    Changes during the classes.

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -4,3 +4,31 @@ function SayHello(){
     let new_value = "Hello world!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
 }
+
+var fun:any = SayHello;
+var numer:number = 21;
+var nombre:string = "Lucas";
+
+class Main {
+    public nombre: string;
+    private numero: number;
+
+    constructor(nombre:string, numero:number) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+    mostrar():string {
+        return this.nombre + " " + this.numero;
+    }
+}
+
+var miObjeto:Main = new Main("Lucas", 21);
+
+// miObjeto.numero = 21;
+// alert(miObjeto.mostrar())
+console.log(miObjeto.mostrar())
+
+window.addEventListener("load", ()=> {
+    fun();
+    SayHello();
+});
